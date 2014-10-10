b66b1f0c1798c473422f5c1c94240a405f7381cd
rednosemonkey <info@rednosemonkey.com>
2014-10-10 16:09:18 +0900

    setup express and added html server to server.js

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-var io;
-io = require('socket.io').listen(3000);
+var express  = require("express");
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = 8001;
+
+app.engine('.html', require('ejs').__express); 
+app.set('view engine', 'html'); 
+app.set('view options', { layout: false });
+app.use('/public', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('draw');
+});
+
 io.sockets.on('connection', function(socket) {
   socket.on('banana', function(data) {
-	    socket.broadcast.emit('apple', data);
-  });
+    socket.broadcast.emit('apple', data);
+	});
+});
+
+server.listen(port, function(){
+  console.log("Listening on port " + port);
 });
